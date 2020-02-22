d77117bb2528f93f721eda52d15c88a98f5dcfc0
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-22 21:49:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
    text = "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary" 
-   text.scan (/\w+un=ing/)
+   text.scan (/un=ing/)
 end
 
 def words_five_letters_long(text)
