6b0d33fd03d6e428b7bff7146be310bd725077e1
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-22 21:24:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 word =  "a foot Excellent incredible Absolute unreal Inconceivable octopus"
 def starts_with_a_vowel?(word)
- word =~ /[aeiou]/
+ word ==~ /[aeiou]/
  return true 
 end
 if word !=~ /[aeiou]/
