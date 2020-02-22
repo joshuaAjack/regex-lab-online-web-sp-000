58be4bfd8add37a22b7945c4cafe2ba452c5b8cb
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-22 21:40:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,9 +5,6 @@ def starts_with_a_vowel?(word)
  word =~ /\A(?=[^aeiou])(?=[a-z])/i
  return true
 end
-if word !=~ /[aeiou]/
-  return true
-end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
