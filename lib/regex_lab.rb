bf3e33efe00fea1956b50287c3556c683b57f1cf
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-22 21:51:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,8 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   text = "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary" 
-   text.scan (/un=ing/)
+   text.scan (/un+ing/)
 end
 
 def words_five_letters_long(text)
